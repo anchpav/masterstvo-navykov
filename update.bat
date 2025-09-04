b6639792bdf4e3fb6c2ec213425f968f371c686c
anchpav <anchpav@gmail.com>
2025-09-04 16:46:54 +0300

    ╨Ю╨▒╨╜╨╛╨▓╨╗╨╡╨╜╨╕╨╡ ╤Б╨░╨╣╤В╨░ 04.09.2025 16:46:54,26

--- update.bat
+++ update.bat
@@ -1,7 +1,15 @@
 @echo off
-cd /d "D:\site\habits-workshop"  :: путь к твоему проекту
+REM Перейти в папку проекта
+cd /d "D:\site\habits-workshop"
+
+REM Добавляем все изменения
 git add .
+
+REM Коммит с текущей датой и временем
 git commit -m "Обновление сайта %date% %time%"
+
+REM Пуш в ветку main
 git push origin main
+
 echo Обновление отправлено на GitHub!
 pause
