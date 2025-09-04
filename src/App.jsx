3b6f7cbfa23d7f6c0eafc1420846e064ec730f66
anchpav <anchpav@gmail.com>
2025-09-04 16:11:25 +0300

    Исправил дублирующийся Header и Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Footer from "./components/Footer";
-import EasterEggs from "./components/EasterEggs";
-
 import HomePage from "./pages/HomePage";
 import Planning from "./pages/services/Planning";
 import Communication from "./pages/services/Communication";
@@ -13,9 +8,6 @@ import Consultation from "./pages/services/Consultation";
 export default function App() {
   return (
     <Router>
-      
-      <EasterEggs /> {/* Пасхалки видны на всех страницах */}
-      <Hero />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/services/planning" element={<Planning />} />
@@ -23,7 +15,6 @@ export default function App() {
         <Route path="/services/challenge" element={<Challenge />} />
         <Route path="/services/consultation" element={<Consultation />} />
       </Routes>
-      
     </Router>
   );
 }
